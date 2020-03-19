e1dd1aef7796d078821d2061d690a0e6d2ebb8b8
xzxiaoshan <365384722@qq.com>
2020-03-19 13:11:14 +0800

    Update createCmd.sh

--- shell/createCmd.sh
+++ shell/createCmd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #CERT_PARAMS="--email 365384722@qq.com -d xxx.com -d *.xxx.com"
 echo $CERT_PARAMS
-CMD="echo 'Y' | /usr/local/bin/certbot certonly ${CERT_PARAMS} --agree-tos --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory --manual --manual-public-ip-logging-ok --manual-auth-hook '/opt/certbot-au/au.sh python ${PDNS} add' --manual-cleanup-hook '/opt/certbot-au/au.sh python ${PDNS} clean'"
+CMD="echo 'Y' | /usr/local/bin/certbot-auto certonly ${CERT_PARAMS} --agree-tos --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory --manual --manual-public-ip-logging-ok --manual-auth-hook '/opt/certbot-au/au.sh python ${PDNS} add' --manual-cleanup-hook '/opt/certbot-au/au.sh python ${PDNS} clean'"
 eval $CMD
