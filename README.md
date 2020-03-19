2688eac6a89179b4158ca118134902c632033b6f
shanhy <xz.xiaoshan@gmail.com>
2020-03-19 13:27:00 +0800

    修改日志路径

--- README.md
+++ README.md
@@ -5,10 +5,8 @@ certbot 免费泛域名证书的生成，容器启动自动生成，容器自动
 ### 使用方法 
 **命令代码**  
 ```
-touch /opt/certbot/logs/certbot.log
 docker run -itd --name xzxiaoshan-certbot \
 -v /opt/certbot/logs:/var/log/letsencrypt \
--v /opt/certbot/logs/certbot.log:/var/log/certbot.log \
 -v /opt/certbot/letsencrypt:/etc/letsencrypt \
 -e ALY_TOKEN=XXXXXXXXXX \
 -e ALY_KEY=XXXXXXXXX -e PDNS=aly \
--- shell/letsCert_inside.sh
+++ shell/letsCert_inside.sh
@@ -2,9 +2,9 @@
 count=`ls /etc/letsencrypt/renewal|grep **.conf|wc -w`
 if [ "$count" -gt "0" ];
 then
-  echo "[`date +%Y-%m-%dT%H:%M:%S`] Exec renewCmd" >> /var/log/certbot.log
-  sh /opt/shell/renewCmd.sh >> /var/log/certbot.log 2>&1
+  echo "[`date +%Y-%m-%dT%H:%M:%S`] Exec renewCmd" >> /var/log/letsencrypt/certbot.log
+  sh /opt/shell/renewCmd.sh >> /var/log/letsencrypt/certbot.log 2>&1
 else
-  echo "[`date +%Y-%m-%dT%H:%M:%S`] Exec createCmd" >> /var/log/certbot.log
-  sh /opt/shell/createCmd.sh >> /var/log/certbot.log 2>&1
+  echo "[`date +%Y-%m-%dT%H:%M:%S`] Exec createCmd" >> /var/log/letsencrypt/certbot.log
+  sh /opt/shell/createCmd.sh >> /var/log/letsencrypt/certbot.log 2>&1
 fi
