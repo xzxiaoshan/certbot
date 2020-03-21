c0612f7f1ca1cba9504c1676a1291c7eb8bf7c80
xzxiaoshan <365384722@qq.com>
2020-03-21 13:55:21 +0800

    Update entrypoint.sh

--- shell/entrypoint.sh
+++ shell/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir -p /etc/letsencrypt/synology && \cp -p /opt/shell/syncSynologyCert.sh /etc/letsencrypt/synology/
 crond start && /opt/shell/letsCert_inside.sh & 
 tail -f /dev/null
